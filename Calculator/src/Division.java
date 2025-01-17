ecb7ccef1009aabf47a0930632b3c22396069e80
Krishna <krishnaag866@gmail.com>
2025-01-17 14:44:40 +0530

    second commit

--- Calculator/src/Division.java
+++ Calculator/src/Division.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class DAY2_Div {
+public class Division {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
